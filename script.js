12c537bc9875ccb78a2da058da8c84f8694f6c2c
Pedro Vinicius do Nascimento Silva <149546910+Pedro-Vino@users.noreply.github.com>
2024-01-13 11:57:27 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function pedro(){
+var pedregulho = document.getElementById("bolotinha").value;
+    var pedrosa = document.getElementById("bolota").value;
+    if(pedrosa==""){
+        window.alert("Digite um email");
+        return false;
+    }   
+    else if(pedrosa.indexOf("@")== -1){
+        window.alert("escreve um @");
+        return false;
+    }
+    else if(pedrosa.indexOf(".")== -1){
+        window.alert("escreve um .");
+        return false;
+    }
+    else if(pedregulho==""){
+        window.alert("Digite uma senha");
+        return false;
+    }
+    else{
+        window.alert("cadastro ok");
+        return true;
+    }
+}
