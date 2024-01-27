2e25c9225b7a292426f7d89e467ac8faacccf1fe
Pedro Vinicius do Nascimento Silva <149546910+Pedro-Vino@users.noreply.github.com>
2024-01-27 12:27:01 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+/*
+Verificar email/senha
+*/
 function pedro(){
 var pedregulho = document.getElementById("bolotinha").value;
     var pedrosa = document.getElementById("bolota").value;
@@ -20,5 +23,33 @@ var pedregulho = document.getElementById("bolotinha").value;
     else{
         window.alert("Cadastro ok!");
         return true;
+    }
+}
+/*
+pesquisar
+*/
+var btn = document.querySelector('#search');
+var container = document.querySelector('#pesquisa');
+
+btn.addEventListener('click', function(){
+    if(container.style.display === 'flex'){
+        container.style.display = 'none';
+    } else{
+        container.style.display = 'flex';
+    }
+});
+
+
+function search(){
+    let input = document.getElementById('txtBusca').value
+    input = input.toLowerCase()
+    let x = document.getElementsByClassName('categorias')
+
+    for(i = 0; i < x.length; i++){
+        if(!x[i].innerHTML.toLowerCase().includes(input)){
+        x[i].style.display = "none"
+    }else{
+        x[i].style.display = "list-item"
+    }
     }
 }
